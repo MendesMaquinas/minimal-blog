5d6e79e40a68e2a1da963a24a70bf0cc0d751c6c
rafael.gois <api.mendes@mendesmaquinas.com.br>
2024-06-03 13:37:43 -0300

    up

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { ArrowLeftCircleIcon } from "@heroicons/react/16/solid";
+import { ArrowLeftIcon } from "@heroicons/react/24/solid";
 import { getArticleData } from "@/lib/articles";
 
 const Article = async ({params}: {params: {slug: string}}) => {
@@ -8,9 +8,9 @@ const Article = async ({params}: {params: {slug: string}}) => {
     return (
         <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
             <div className="flex justify-between font-poppins">
-                <Link href="/">
-                    <ArrowLeftCircleIcon className="w-6 h-6 text-gray-500" />
-                    <p>back to home</p>
+                <Link href="/" className="flex items-center gap-2">
+                    <ArrowLeftIcon className="w-6 h-6 text-gray-900" />
+                    <p className="text-xs">back to home</p>
                 </Link>
                 <p>{articleData.date.toString()}</p>
             </div>
